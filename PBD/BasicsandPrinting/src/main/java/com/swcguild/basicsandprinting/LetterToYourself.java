e68c13183de8d6110942dc2e35220585c6d0f5a9
Andria Fuquen <andria.fuquen@gmail.com>
2015-04-15 22:40:57 -0400

    all of basics and printing

---
+++ PBD/BasicsandPrinting/src/main/java/com/swcguild/basicsandprinting/LetterToYourself.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.swcguild.basicsandprinting;
+
+/**
+ *
+ * @author apprentice
+ */
+public class LetterToYourself {
+    public static void main(String[] args) {
+        System.out.println("+---------------------------------------------------------+");
+    System.out.println("|                                                    #### |");
+    System.out.println("|                                                    #### |");
+System.out.println("|                                                    #### |");
+System.out.println("|                                                         |");
+System.out.println("|                                                         |");
+System.out.println("|                              Toodles                    |");
+System.out.println("|                              1 Microsoft Way            |");
+System.out.println("|                              Redmond, WA 98104          |");
+System.out.println("|                                                         |");
+System.out.println("+---------------------------------------------------------+");
+    }
+}
---
+++ PBD/BasicsandPrinting/src/main/java/com/swcguild/basicsandprinting/YourInitials.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.swcguild.basicsandprinting;
+
+/**
+ *
+ * @author apprentice
+ */
+public class YourInitials {
+    public static void main(String[] args) {
+    System.out.println("  A    EEEEE   FFFFF");
+    System.out.println(" A  A  E       F");
+    System.out.println("A   A  EEE     FFF");
+    System.out.println("AAAAA  E       F"); 
+    System.out.println("A   A  E       F");
+    System.out.println("A   A  EEEEE   F");
+        
+        
+    }
+    
+}
