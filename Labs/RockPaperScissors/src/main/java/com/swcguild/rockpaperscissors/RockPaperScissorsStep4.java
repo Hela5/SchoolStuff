eae922b23fe659524a723d8cf5ce91b2ea88a321
Andria Fuquen <andria.fuquen@gmail.com>
2015-04-20 16:13:01 -0400

    refactored version with methods

--- Labs/RockPaperScissors/src/main/java/com/swcguild/rockpaperscissors/RockPaperScissorsStep4.java
+++ Labs/RockPaperScissors/src/main/java/com/swcguild/rockpaperscissors/RockPaperScissorsStep4.java
@@ -24,79 +24,33 @@ public class RockPaperScissorsStep4 {
         while (playAgain) {
             playAgain = false;
             System.out.println("Welcome to Rock! Paper! Scissors!");
+            
             do {
                 System.out.println("How many rounds would you like to play today?");
                 System.out.println("Minium rounds: 1, Maximum rounds: 10");
                 stringNumRounds = sc.nextLine();
                 numRounds = Integer.parseInt(stringNumRounds);
             } while (numRounds < 1 || numRounds > 10);
+            
             for (int i = 1; i < numRounds + 1; i++) {
-                System.out.println("Please choose from the following: ");
-                System.out.println("\t1 = Rock");
-                System.out.println("\t2 = Paper");
-                System.out.println("\t3 = Scissors");
-                System.out.print("Choose your weapon! ");
-                stringUserVal = sc.nextLine();
+                userVal = displayMenu();
 
-                userVal = Integer.parseInt(stringUserVal);
-
-                //for (int j = 0; j < 30; j++) {
                 compVal = rand.nextInt(3) + 1;
-                System.out.println(compVal);
-                //}
-
-                if (compVal == 1 && userVal == 1) {
-                    System.out.println("It's a tie!");
-                    ties++;
-                }
-                if (compVal == 2 && userVal == 2) {
-                    System.out.println("It's a tie!");
-                    ties++;
-                }
-                if (compVal == 3 && userVal == 3) {
-                    System.out.println("It's a tie!");
-                    ties++;
-                }
-
-                if (compVal == 1 && userVal == 2) {
-                    System.out.println("User wins! Paper wraps rock");
-                    userWins++;
-                }
-                if (compVal == 2 && userVal == 1) {
-                    System.out.println("Computer Wins! Paper wraps rock");
-                    compWins++;
-                }
-                if (compVal == 1 && userVal == 3) {
-                    System.out.println("Computer Wins! Rock smashes scissors");
-                    compWins++;
-                }
-                if (compVal == 3 && userVal == 1) {
-                    System.out.println("User Wins! Rock smashes scissors");
-                    userWins++;
-                }
-                if (compVal == 2 && userVal == 3) {
-                    System.out.println("User Wins! Scissors cut paper");
-                    userWins++;
-                }
-                if (compVal == 3 && userVal == 2) {
-                    System.out.println("Computer Wins! Scissors cut paper");
-                    compWins++;
-                }
-                if (userVal == 4) {
-                    System.out.println("Nuclear Option! Everyone loses!!");
-                }
+                System.out.println("Computer picks " + compVal + "\n");
+    
+                int winnerNum = checkWinner(userVal, compVal);
+                    if (winnerNum == 2) {
+                        ties++;
+                    } else if (winnerNum == 1) {
+                        userWins++;
+                    } else if (winnerNum == 0) {
+                        compWins++;
+                    }
+                
                 System.out.println("   ");
             }
-            System.out.println("Number of ties = " + ties);
-            System.out.println("Number of User Wins = " + userWins);
-            System.out.println("Number of Computer Wins = " + compWins);
-            if (userWins == compWins) {
-                System.out.println("It's a tie!");
-            } else if (userWins > compWins) {
-                System.out.println("User Beat the Computer!");
-            } else {
-                System.out.println("The Computer beat User!");
-            } 
+            checkWinStats (ties, userWins, compWins);
+            
             System.out.println("\n Do you want to play again? Yes or No?");
             String stringPlayAgain = sc.nextLine();
             if (stringPlayAgain.equals("Yes")) {
@@ -106,4 +60,65 @@ public class RockPaperScissorsStep4 {
             }
         }
     }
+
+    public static int displayMenu() {
+        Scanner sc = new Scanner(System.in);
+        String stringUserVal = "";
+        int userVal = 0;
+
+        System.out.println("Please choose from the following: ");
+        System.out.println("\t1 = Rock");
+        System.out.println("\t2 = Paper");
+        System.out.println("\t3 = Scissors");
+        System.out.print("Choose your weapon! ");
+        stringUserVal = sc.nextLine();
+
+        userVal = Integer.parseInt(stringUserVal);
+        return userVal;
+    }
+
+    
+    
+    public static int checkWinner(int userVal, int compVal) {
+        int winnerType = 0;
+        
+        if (compVal == userVal) {
+            System.out.println("It's a tie!");
+            winnerType = 2;
+        } else if (compVal == 1 && userVal == 2) {
+            System.out.println("User wins! Paper wraps rock");
+            winnerType = 1;
+        } else if (compVal == 2 && userVal == 1) {
+            System.out.println("Computer Wins! Paper wraps rock");
+            winnerType = 0;
+        } else if (compVal == 1 && userVal == 3) {
+            System.out.println("Computer Wins! Rock smashes scissors");
+            winnerType = 0;
+        } else if (compVal == 3 && userVal == 1) {
+            System.out.println("User Wins! Rock smashes scissors");
+            winnerType = 1;
+        } else if (compVal == 2 && userVal == 3) {
+            System.out.println("User Wins! Scissors cut paper");
+            winnerType = 1;
+        } else if (compVal == 3 && userVal == 2) {
+            System.out.println("Computer Wins! Scissors cut paper");
+            winnerType = 0;
+        }
+        return winnerType;
+    }
+    
+    
+    public static void checkWinStats(int ties, int userWins, int compWins) {
+        System.out.println("Number of ties = " + ties);
+            System.out.println("Number of User Wins = " + userWins);
+            System.out.println("Number of Computer Wins = " + compWins);
+            if (userWins == compWins) {
+                System.out.println("It's a tie!");
+            } else if (userWins > compWins) {
+                System.out.println("The User Beat the Computer!");
+            } else {
+                System.out.println("The Computer beat the User!");
+            }
+    }
+    
 }
